468ea4c4d0cef664b3d6fbee5504e98c6cd413d7
Devon Milley <78571858+DevonMilley@users.noreply.github.com>
2023-05-10 20:49:53 +0000

    got all content to appear

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 pip install google-api-python-client bs4 requests google-auth google-auth-oauthlib google-auth-httplib2 aiohttp datetime flask pandas sqlite-utils peewee
 
-sqlite-utils insert stylebook.db style_tips static/dbk_stylebook.csv --csv
+rm stylebook.db
 
+sqlite-utils insert stylebook.db style_tips static/dbk_stylebook.csv --csv
 
 #to run, 'sh setup.sh'
