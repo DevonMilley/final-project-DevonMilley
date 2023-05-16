182eb287b77d56799eeb2e56328308b063f13076
Devon Milley <78571858+DevonMilley@users.noreply.github.com>
2023-05-16 22:07:48 +0000

    working on fts

--- app.py
+++ app.py
@@ -5,32 +5,53 @@ from google.oauth2 import service_account
 from googleapiclient.discovery import build
 import json
 import requests
-from flask import Flask
+from flask import Flask, render_template, request, flash, redirect, url_for
+from playhouse.sqlite_ext import *
 from flask import render_template
 from peewee import *
 
 app = Flask(__name__)
-#app.secret_key = "279878dhekdhkhekdhkh" #can i use the same api key for this
-#db = SqliteDatabase('foreclosures.db')
+app.secret_key = "279878dhekdhkhekdhkh" #can i use the same api key for this
 
-db = SqliteDatabase('stylebook.db')
+db = SqliteExtDatabase('stylebook.db')
 
 
 class Stylebook(Model):
     id = IntegerField(unique=True)
     name = CharField()
-    description = CharField()
+    description = TextField()
     tags = CharField()
 
     class Meta:
         table_name = "style_tips"
         database = db
 
-@app.route("/")
+class StylebookIndex(FTSModel):
+    rowid = RowIDField()
+    description = SearchField()
+
+    class Meta:
+        database = db
+        options = {'tokenize': 'porter', 'content': Stylebook.description}
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    all_tips = Stylebook.select()
-    template = 'index.html'
-    return render_template(template, all_tips = all_tips)
+    if request.method == 'POST':
+        search_term = request.form['search_term']
+        results = (Stylebook.select(Stylebook, StylebookIndex.rank()).join(StylebookIndex,on=(Stylebook.id == StylebookIndex.rowid)).where(StylebookIndex.match(search_term)).order_by(StylebookIndex.rank()))
+        document_count = len(list(set([x.url for x in results])))
+        if document_count > 0:
+            recent = results.get()
+        else:
+            recent = Stylebook.select().get()
+    else:
+        results = None
+        search_term = None
+        document_count = Stylebook.select().count()
+        recent = Stylebook.select().get()
+        all_tips = Stylebook.select()
+        template = 'index.html'
+    return render_template(template, all_tips = all_tips, results = results, document_count=document_count, search_term = search_term, recent=recent)
 
 
 
